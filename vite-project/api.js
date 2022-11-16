53299f83c60ceb378438ec1e9c527993e56ac6f6
tonitruum-0 <104451675+tonitruum-0@users.noreply.github.com>
2022-11-16 13:15:10 -0500

    asdadsasdasdasdas

--- vite-project/api.js
+++ vite-project/api.js
@@ -2,43 +2,42 @@ let data = [];
 let html = "";
 
 let DOMSelectors = {
-  container: document.getElementById('container'),
-}
+  container: document.getElementById("container"),
+};
 
 export async function getCats() {
-  let api_url = `https://cataas.com/api/cats?limit=1114`
+  let api_url = `https://cataas.com/api/cats?limit=1114`;
   let response = await fetch(api_url);
   data = await response.json();
   console.log(response);
-/*   document.getElementById('container').innerHTML = "";
- */}
-
-
+}
 
 export async function makeHtml(numCats) {
   for (let i = 0; i < numCats; i++) {
     let id = getRandom(data, numCats)[i]._id;
     console.log(id);
-    html += `<img src='https://cataas.com/cat/${id}' id='cat${i+1}'>`
+    html += `<img src='https://cataas.com/cat/${id}' id='cat${
+      i + 1
+    }' class='cats'>`;
+    console.log(html);
   }
 }
 
-export async function makeCats(numCats) {
-    DOMSelectors.container.innerHTML = html;
-    html = "";
-  }
-//https://stackoverflow.com/questions/11071314/javascript-execute-after-all-images-have-loaded
+export async function makeCats() {
+  DOMSelectors.container.innerHTML = html;
+  html = "";
+}
 
 function getRandom(arr, n) {
   let result = new Array(n),
-      len = arr.length,
-      taken = new Array(len);
+    len = arr.length,
+    taken = new Array(len);
   if (n > len)
-      throw new RangeError("getRandom: more elements taken than available");
+    throw new RangeError("getRandom: more elements taken than available");
   while (n--) {
-      let x = Math.floor(Math.random() * len);
-      result[n] = arr[x in taken ? taken[x] : x];
-      taken[x] = --len in taken ? taken[len] : len;
+    let x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
   }
   return result;
 }
--- vite-project/main.js
+++ vite-project/main.js
@@ -2,27 +2,33 @@ import "./style.css";
 import * as api from "./api.js";
 
 let DOMSelectors = {
-    container: document.getElementById('container'),
-}
+  container: document.getElementById("container"),
+};
 
 await api.getCats();
 await api.makeHtml(10);
 await api.makeCats(10);
 
-Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
-    console.log('images finished loading');
-    document.getElementById('container').style.backgroundColor = 'green';
+Promise.all(
+  Array.from(document.images)
+    .filter((img) => !img.complete)
+    .map(
+      (img) =>
+        new Promise((resolve) => {
+          img.onload = img.onerror = resolve;
+        })
+    )
+).then(() => {
+  console.log("images finished loading");
 });
 
 form.addEventListener("submit", function (e) {
-    let numCats = 0;
-    e.preventDefault();
-    let input = document.getElementById('num');
-    numCats = input.value;
-    console.log(input.value);
-    input.value = "";
-    api.makeHtml(numCats);
-    api.makeCats(numCats);
-    document.getElementById('container').style.backgroundColor = 'red';
+  let numCats = 0;
+  e.preventDefault();
+  let input = document.getElementById("num");
+  numCats = input.value;
+  console.log(input.value);
+  input.value = "";
+  api.makeHtml(numCats);
+  api.makeCats();
 });
-
