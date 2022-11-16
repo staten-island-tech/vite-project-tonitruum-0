9cbe1219bfcf743d26ede6a0246e32a099ea333c
tonitruum-0 <104451675+tonitruum-0@users.noreply.github.com>
2022-11-16 00:12:04 -0500

    asasdasd

--- vite-project/api.js
+++ vite-project/api.js
@@ -1,8 +1,31 @@
-let limit = 10;
-let api_url = `https://cataas.com/api/cats?limit=${limit}`
+let data = [];
+let html = "";
 
-export async function getCats() {
-  let response = await fetch(api_url);
-  let data = await response.json();
-  console.log(data);
+let DOMSelectors = {
+  container: document.getElementById('container'),
 }
+
+export async function getCats(numCats) {
+  let api_url = `https://cataas.com/api/cats?limit=${numCats}`
+  let response = await fetch(api_url);
+  data = await response.json();
+  console.log(data[1]._id);
+/*   document.getElementById('container').innerHTML = "";
+ */}
+
+console.log(data);
+
+export async function makeHtml(numCats) {
+  for (let i = 0; i < numCats; i++) {
+    let id = data[i]._id;
+    html += `<img src='https://cataas.com/cat/${id}' id='cat${i+1}'>`
+  }
+}
+
+export async function makeCats() {
+  alert(html);
+    DOMSelectors.container.innerHTML = html;
+    html = "";
+}
+
+//https://stackoverflow.com/questions/11071314/javascript-execute-after-all-images-have-loaded
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,26 @@
 import "./style.css";
 import * as api from "./api.js";
 
-api.getCats();
+let DOMSelectors = {
+    container: document.getElementById('container'),
+}
+// runs function once initially to display 10 cats
+await api.getCats(10);
+await api.makeHtml(10);
+await api.makeCats();
+
+form.addEventListener("submit", function (e) {
+    let numCats = 0;
+    // prevents reload
+    e.preventDefault();
+    // input field
+    let input = document.getElementById('num');
+    // sets the number of cats user wants displayed
+    numCats = input.value;
+    console.log(input.value);
+    // clears form
+    input.value = "";
+    // gets cat data from api
+    api.getCats(numCats);
+    api.makeCats(numCats);
+});
